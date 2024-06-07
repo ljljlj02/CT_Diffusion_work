30545f37166815ab91aa9f5c3ff394e4651857e9
JL <50227846+ljljlj02@users.noreply.github.com>
2024-06-07 15:01:19 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,35 +1,15 @@
-from gamma import gamma_index
-
-def compute_gamma(reference, evaluation, dose_threshold=0.1, dose_diff=0.03, dist_mm=3.0):
-    reference = reference.numpy()
-    evaluation = evaluation.numpy()
-    gamma_map = gamma_index(reference, evaluation, dose_threshold, dose_diff, dist_mm)
-    return gamma_map
-
-
-def evaluate_model(model, dataloader, device):
+def evaluate_model(model, test_images, timesteps):
     model.eval()
-    all_gamma_maps = []
     with torch.no_grad():
-        for noisy_imgs, clean_imgs in dataloader:
-            noisy_imgs = noisy_imgs.to(device)
-            clean_imgs = clean_imgs.to(device)
-            outputs = model(noisy_imgs)
+        denoised_images = []
+        for i in range(test_images.shape[0]):
+            noisy_img = forward_diffusion(test_images[i:i+1], timesteps, alpha)[-1]
+            denoised_img = backward_denoising(model, [noisy_img], timesteps)
+            denoised_images.append(denoised_img)
+        return np.array(denoised_images)
 
-            for ref, eval in zip(clean_imgs, outputs):
-                gamma_map = compute_gamma(ref.squeeze(), eval.squeeze())
-                all_gamma_maps.append(gamma_map)
+test_clean_images = load_nifti('test_clean_images.nii.gz')
 
-    return all_gamma_maps
+denoised_images = evaluate_model(model, test_clean_images, timesteps)
+print("Denoising completed.")
 
-
-# 加载测试数据
-test_noisy_image_paths = ['path/to/test_noisy_image1.nii', 'path/to/test_noisy_image2.nii', ...]
-test_clean_image_paths = ['path/to/test_clean_image1.nii', 'path/to/test_clean_image2.nii', ...]
-test_dataset = CTNiftiDataset(test_noisy_image_paths, test_clean_image_paths, transform=transform)
-test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=False)
-
-# 评估模型
-gamma_maps = evaluate_model(model, test_dataloader, device)
-mean_gamma = np.mean([np.mean(gamma_map) for gamma_map in gamma_maps])
-print(f'Gamma 3%/3mm/10% mean value: {mean_gamma:.4f}')
